5153bc192a70b77e573347afc03570a68af15ad2
McJty <mcjty1@gmail.com>
2016-01-27 12:01:45 +0100

    Added /rftdb createdimlet command to create any dimlet

--- src/main/java/mcjty/rftoolsdim/commands/CmdCreateDimlet.java
+++ src/main/java/mcjty/rftoolsdim/commands/CmdCreateDimlet.java
@@ -1,28 +1,35 @@
 package mcjty.rftoolsdim.commands;
 
+import mcjty.lib.container.InventoryHelper;
+import mcjty.rftoolsdim.blocks.enscriber.DimensionEnscriberTileEntity;
 import mcjty.rftoolsdim.config.DimletRules;
 import mcjty.rftoolsdim.config.Settings;
+import mcjty.rftoolsdim.dimensions.dimlets.DimletDebug;
+import mcjty.rftoolsdim.dimensions.dimlets.DimletKey;
+import mcjty.rftoolsdim.dimensions.dimlets.KnownDimletConfiguration;
 import mcjty.rftoolsdim.dimensions.dimlets.types.DimletType;
 import net.minecraft.command.ICommandSender;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.ChatComponentText;
 import net.minecraft.util.EnumChatFormatting;
 
 import java.util.Collections;
 
-public class CmdTestDimlet extends AbstractRfToolsCommand {
+public class CmdCreateDimlet extends AbstractRfToolsCommand {
     @Override
     public String getHelp() {
-        return "<type> <mod> <name>";
+        return "<type> <name>";
     }
 
     @Override
     public String getCommand() {
-        return "testdimlet";
+        return "createdimlet";
     }
 
     @Override
     public int getPermissionLevel() {
-        return 0;
+        return 2;
     }
 
     @Override
@@ -32,21 +39,26 @@ public class CmdTestDimlet extends AbstractRfToolsCommand {
 
     @Override
     public void execute(ICommandSender sender, String[] args) {
-        if (args.length != 4) {
+        if (args.length != 3) {
             sender.addChatMessage(new ChatComponentText(EnumChatFormatting.RED + "Bad parameters!"));
             return;
         }
 
         String typeString = fetchString(sender, args, 1, "material");
-        String mod = fetchString(sender, args, 2, "minecraft");
-        String name = fetchString(sender, args, 3, "");
+        String name = fetchString(sender, args, 2, "");
         DimletType type = DimletType.getTypeByName(typeString);
         if (type == null) {
             sender.addChatMessage(new ChatComponentText(EnumChatFormatting.RED + "Bad type!"));
             return;
         }
 
-        Settings settings = DimletRules.getSettings(type, mod, name, Collections.emptySet(), 0, Collections.emptyMap());
-        sender.addChatMessage(new ChatComponentText(EnumChatFormatting.GREEN + settings.toString()));
+        if (!(sender instanceof EntityPlayer)) {
+            sender.addChatMessage(new ChatComponentText(EnumChatFormatting.RED + "This command only works as a player!"));
+            return;
+        }
+
+        EntityPlayer player = (EntityPlayer) sender;
+        ItemStack dimlet = KnownDimletConfiguration.getDimletStack(new DimletKey(type, name));
+        InventoryHelper.mergeItemStack(player.inventory, false, dimlet, 0, 35, null);
     }
 }
--- src/main/java/mcjty/rftoolsdim/commands/CommandRftDb.java
+++ src/main/java/mcjty/rftoolsdim/commands/CommandRftDb.java
@@ -5,6 +5,7 @@ public class CommandRftDb extends DefaultCommand {
     public CommandRftDb() {
         super();
         registerCommand(new CmdTestDimlet());
+        registerCommand(new CmdCreateDimlet());
         registerCommand(new CmdListDimlets());
         registerCommand(new CmdListBlocks());
         registerCommand(new CmdListLiquids());
