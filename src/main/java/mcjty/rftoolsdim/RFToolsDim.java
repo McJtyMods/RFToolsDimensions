b3ce2c57898f86af8069d6b5e072257d48e28574
McJty <mcjty1@gmail.com>
2016-04-07 14:47:09 +0200

    4.23beta33

--- src/main/java/mcjty/rftoolsdim/RFToolsDim.java
+++ src/main/java/mcjty/rftoolsdim/RFToolsDim.java
@@ -41,10 +41,10 @@ import java.util.EnumMap;
         version = RFToolsDim.VERSION)
 public class RFToolsDim implements ModBase {
     public static final String MODID = "rftoolsdim";
-    public static final String VERSION = "4.23beta32";
+    public static final String VERSION = "4.23beta33";
     public static final String MIN_FORGE_VER = "12.16.0.1835";
-    public static final String MIN_MCJTYLIB_VER = "1.9.0-1.8.1beta13";
-    public static final String MIN_RFTOOLS_VER = "1.9.0-4.23beta39";
+    public static final String MIN_MCJTYLIB_VER = "1.9.0-1.8.1beta14";
+    public static final String MIN_RFTOOLS_VER = "1.9.0-4.23beta40";
 
     @SidedProxy(clientSide="mcjty.rftoolsdim.proxy.ClientProxy", serverSide="mcjty.rftoolsdim.proxy.ServerProxy")
     public static CommonProxy proxy;
