afb43ef62a93f1d47d79b5aaaa0607787b2b888e
McJty <mcjty1@gmail.com>
2017-07-10 14:29:12 +0200

    5.02beta3

--- src/main/java/mcjty/rftoolsdim/ModCrafting.java
+++ src/main/java/mcjty/rftoolsdim/ModCrafting.java
@@ -1,22 +1,14 @@
 package mcjty.rftoolsdim;
 
-import mcjty.rftoolsdim.config.GeneralConfiguration;
-import mcjty.rftoolsdim.dimensions.dimlets.DimletObjectMapping;
 import mcjty.rftoolsdim.dimensions.dimlets.KnownDimletConfiguration;
 import mcjty.rftoolsdim.dimensions.dimlets.types.DimletType;
 import mcjty.rftoolsdim.items.ModItems;
-import mcjty.rftoolsdim.varia.MyGameReg;
-import net.minecraft.block.Block;
 import net.minecraft.enchantment.Enchantment;
 import net.minecraft.enchantment.EnchantmentHelper;
-import net.minecraft.init.Blocks;
 import net.minecraft.init.Enchantments;
 import net.minecraft.init.Items;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.crafting.Ingredient;
-import net.minecraft.item.crafting.ShapedRecipes;
-import net.minecraft.util.NonNullList;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fml.common.registry.ForgeRegistries;
 
@@ -63,26 +55,6 @@ public class ModCrafting {
     }
 
     private static void initDimletRecipes() {
-        Block redstoneTorch = Blocks.REDSTONE_TORCH;
-        addRecipe(DimletType.DIMLET_EFFECT, DimletObjectMapping.NONE_ID, " r ", "rwr", "ppp", 'r', Items.REDSTONE, 'w', Items.APPLE, 'p', Items.PAPER);
-        addRecipe(DimletType.DIMLET_FEATURE, DimletObjectMapping.NONE_ID, " r ", "rwr", "ppp", 'r', Items.REDSTONE, 'w', Items.STRING, 'p', Items.PAPER);
-        addRecipe(DimletType.DIMLET_STRUCTURE, DimletObjectMapping.NONE_ID, " r ", "rwr", "ppp", 'r', Items.REDSTONE, 'w', Items.BONE, 'p', Items.PAPER);
-        addRecipe(DimletType.DIMLET_TERRAIN, "Void", " r ", "rwr", "ppp", 'r', Items.REDSTONE, 'w', Items.BRICK, 'p', Items.PAPER);
-        if (!GeneralConfiguration.voidOnly) {
-            addRecipe(DimletType.DIMLET_TERRAIN, "Flat", " r ", "rwr", "ppp", 'r', Items.REDSTONE, 'w', Items.BRICK, 'p', ModItems.dimletTemplateItem);
-        }
-        addRecipe(DimletType.DIMLET_CONTROLLER, DimletObjectMapping.DEFAULT_ID, " r ", "rwr", "ppp", 'r', Items.REDSTONE, 'w', Items.COMPARATOR, 'p', Items.PAPER);
-        addRecipe(DimletType.DIMLET_CONTROLLER, "Single", " r ", "rwr", "ppp", 'r', Items.REDSTONE, 'w', Items.COMPARATOR, 'p', ModItems.dimletTemplateItem);
-        addRecipe(DimletType.DIMLET_MATERIAL, Blocks.STONE.getRegistryName() + "@0", " r ", "rwr", "ppp", 'r', Items.REDSTONE, 'w', Blocks.DIRT, 'p', Items.PAPER);
-        addRecipe(DimletType.DIMLET_LIQUID, Blocks.WATER.getRegistryName() + "@0", " r ", "rwr", "ppp", 'r', Items.REDSTONE, 'w', Items.BUCKET, 'p', Items.PAPER);
-        addRecipe(DimletType.DIMLET_SKY, "Normal", " r ", "rwr", "ppp", 'r', Items.REDSTONE, 'w', Items.FEATHER, 'p', ModItems.dimletTemplateItem);
-        addRecipe(DimletType.DIMLET_SKY, "Normal Day", " r ", "rwr", "ppp", 'r', Items.REDSTONE, 'w', Items.GLOWSTONE_DUST, 'p', ModItems.dimletTemplateItem);
-        addRecipe(DimletType.DIMLET_SKY, "Normal Night", " r ", "rwr", "ppp", 'r', Items.REDSTONE, 'w', Items.COAL, 'p', Items.PAPER);
-        addRecipe(DimletType.DIMLET_MOB, DimletObjectMapping.DEFAULT_ID, " r ", "rwr", "ppp", 'r', Items.REDSTONE, 'w', Items.ROTTEN_FLESH, 'p', ModItems.dimletTemplateItem);
-        addRecipe(DimletType.DIMLET_TIME, "Normal", " r ", "rwr", "ppp", 'r', Items.REDSTONE, 'w', Items.CLOCK, 'p', ModItems.dimletTemplateItem);
-        addRecipe(DimletType.DIMLET_WEATHER, DimletObjectMapping.DEFAULT_ID, " r ", "rwr", "ppp", 'r', Items.REDSTONE, 'w', Items.SNOWBALL, 'p', Items.PAPER);
-        addRecipe(DimletType.DIMLET_DIGIT, "0", " r ", "rtr", "ppp", 'r', Items.REDSTONE, 't', redstoneTorch, 'p', Items.PAPER);
-
         addDigitRecipe("0", "1");
         addDigitRecipe("1", "2");
         addDigitRecipe("2", "3");
@@ -101,9 +73,5 @@ public class ModCrafting {
                 new String[][]{new String[]{"dkey"}},
                 KnownDimletConfiguration.getDimletStack(DimletType.DIMLET_DIGIT, dest))
                 .setRegistryName(new ResourceLocation(RFToolsDim.MODID, "digit" + dest)));
-    }
-
-    private static void addRecipe(DimletType type, String id, Object... params) {
-        MyGameReg.addRecipe(KnownDimletConfiguration.getDimletStack(type, id), params);
     }
 }
