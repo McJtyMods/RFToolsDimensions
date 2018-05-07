bda6b02cd117219bfb867a478c6c92cc9006cfbd
McJty <mcjty1@gmail.com>
2018-05-07 11:37:00 +0200

    Fixes and removal of API's that are not used anymore

--- src/api/java/com/brandon3055/draconicevolution/api/IExtendedRFStorage.java
+++
@@ -1,9 +0,0 @@
-package com.brandon3055.draconicevolution.api;
-
-/**
- * Created by Brandon on 6/03/2015.
- */
-public interface IExtendedRFStorage {
-    double getEnergyStored();
-    double getMaxEnergyStored();
-}
