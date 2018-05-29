7efb015cd561718fa2573a582ed48e4aceb7d9b1
McJty <mcjty1@gmail.com>
2018-05-29 09:35:47 +0200

    Fixed versions in all mods

--- src/main/java/mcjty/rftoolsdim/RFToolsDim.java
+++ src/main/java/mcjty/rftoolsdim/RFToolsDim.java
@@ -44,10 +44,10 @@ import java.util.function.Function;
         version = RFToolsDim.VERSION)
 public class RFToolsDim implements ModBase {
     public static final String MODID = "rftoolsdim";
-    public static final String MIN_RFTOOLS_VER = "7.50";
-    public static final String VERSION = "5.60alpha";
+    public static final String MIN_RFTOOLS_VER = "7.50-alpha";
+    public static final String VERSION = "5.60-alpha";
     public static final String MIN_FORGE11_VER = "14.23.2.2645";
-    public static final String MIN_MCJTYLIB_VER = "3.0.0";
+    public static final String MIN_MCJTYLIB_VER = "3.0.0-alpha";
 
     @SidedProxy(clientSide="mcjty.rftoolsdim.proxy.ClientProxy", serverSide="mcjty.rftoolsdim.proxy.ServerProxy")
     public static CommonProxy proxy;
