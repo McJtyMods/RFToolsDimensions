f36636e6cb7fd390b9d39bf1bce98c873713a787
McJty <mcjty1@gmail.com>
2018-10-25 04:52:37 +0200

    5.62

--- src/main/java/mcjty/rftoolsdim/RFToolsDim.java
+++ src/main/java/mcjty/rftoolsdim/RFToolsDim.java
@@ -44,7 +44,7 @@ import java.util.function.Function;
 public class RFToolsDim implements ModBase {
     public static final String MODID = "rftoolsdim";
     public static final String MIN_RFTOOLS_VER = "7.50";
-    public static final String VERSION = "5.61";
+    public static final String VERSION = "5.62";
     public static final String MIN_FORGE11_VER = "14.23.2.2645";
     public static final String MIN_MCJTYLIB_VER = "3.0.0";
 
