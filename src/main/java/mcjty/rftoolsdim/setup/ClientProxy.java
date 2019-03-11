3bde04b86302914b050838772f0f13e34ef21536
McJty <mcjty1@gmail.com>
2019-03-11 11:27:05 +0100

    New ClientRegistration class

--- src/main/java/mcjty/rftoolsdim/setup/ClientProxy.java
+++ src/main/java/mcjty/rftoolsdim/setup/ClientProxy.java
@@ -1,41 +1,7 @@
 package mcjty.rftoolsdim.setup;
 
 import mcjty.lib.setup.DefaultClientProxy;
-import mcjty.rftoolsdim.RFToolsDim;
-import mcjty.rftoolsdim.blocks.ModBlocks;
-import mcjty.rftoolsdim.items.ModItems;
-import net.minecraft.util.ResourceLocation;
-import net.minecraftforge.client.event.ModelRegistryEvent;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.fml.common.event.FMLInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
+@SuppressWarnings("EmptyClass")
 public class ClientProxy extends DefaultClientProxy {
-
-    private static final ResourceLocation VILLAGER_TEXTURE = new ResourceLocation(RFToolsDim.MODID, "textures/entities/rftoolsvillager.png");
-
-    @SubscribeEvent
-    public void registerModels(ModelRegistryEvent event) {
-        ModItems.initClient();
-        ModBlocks.initClient();
-    }
-
-
-    @Override
-    public void preInit(FMLPreInitializationEvent e) {
-        super.preInit(e);
-        MinecraftForge.EVENT_BUS.register(this);
-    }
-
-    @Override
-    public void init(FMLInitializationEvent e) {
-        super.init(e);
-    }
-
-    @Override
-    public void postInit(FMLPostInitializationEvent e) {
-        super.postInit(e);
-    }
 }
