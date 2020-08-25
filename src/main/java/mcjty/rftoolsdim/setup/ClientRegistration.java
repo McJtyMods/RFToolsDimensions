4b6a7d3b7f2f080cc677b90ea0c8b80acd8fc093
mcjty <slto15aGH10>
2020-08-25 13:50:56 +0200

    Safer client-side registrations (thread-safety)

--- src/main/java/mcjty/rftoolsdim/setup/ClientRegistration.java
+++ src/main/java/mcjty/rftoolsdim/setup/ClientRegistration.java
@@ -13,6 +13,7 @@ import mcjty.rftoolsdim.modules.workbench.WorkbenchSetup;
 import mcjty.rftoolsdim.modules.workbench.client.GuiWorkbench;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.DeferredWorkQueue;
 import net.minecraftforge.fml.client.registry.RenderingRegistry;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
@@ -22,9 +23,11 @@ public class ClientRegistration {
 
     @SubscribeEvent
     public static void init(FMLClientSetupEvent event) {
-        GenericGuiContainer.register(DimensionBuilderSetup.CONTAINER_DIMENSION_BUILDER.get(), GuiDimensionBuilder::new);
-        GenericGuiContainer.register(WorkbenchSetup.CONTAINER_WORKBENCH.get(), GuiWorkbench::new);
-        GenericGuiContainer.register(EnscriberSetup.CONTAINER_ENSCRIBER.get(), GuiEnscriber::new);
+        DeferredWorkQueue.runLater(() -> {
+            GenericGuiContainer.register(DimensionBuilderSetup.CONTAINER_DIMENSION_BUILDER.get(), GuiDimensionBuilder::new);
+            GenericGuiContainer.register(WorkbenchSetup.CONTAINER_WORKBENCH.get(), GuiWorkbench::new);
+            GenericGuiContainer.register(EnscriberSetup.CONTAINER_ENSCRIBER.get(), GuiEnscriber::new);
+        });
         RenderingRegistry.registerEntityRenderingHandler(EntitySetup.DIMENSIONAL_BLOB.get(), DimensionalBlobRender.FACTORY);
     }
 }
