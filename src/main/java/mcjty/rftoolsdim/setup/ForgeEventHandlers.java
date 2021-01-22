cb2ca7699c7817087b9619641246657cce4f9931
McJty <mcjty1@gmail.com>
2021-01-22 06:35:27 +0100

    Broken code

--- src/main/java/mcjty/rftoolsdim/setup/ForgeEventHandlers.java
+++ src/main/java/mcjty/rftoolsdim/setup/ForgeEventHandlers.java
@@ -1,14 +1,8 @@
 package mcjty.rftoolsdim.setup;
 
-import mcjty.rftoolsdim.RFToolsDim;
 import mcjty.rftoolsdim.dimension.features.RFTFeature;
-import mcjty.rftoolsdim.modules.blob.BlobModule;
-import net.minecraft.world.IWorld;
-import net.minecraft.world.World;
-import net.minecraft.world.biome.MobSpawnInfo;
 import net.minecraft.world.gen.GenerationStage;
 import net.minecraftforge.event.world.BiomeLoadingEvent;
-import net.minecraftforge.event.world.WorldEvent;
 import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 
@@ -19,15 +13,31 @@ public class ForgeEventHandlers {
         event.getGeneration().getFeatures(GenerationStage.Decoration.RAW_GENERATION).add(() -> RFTFeature.RFTFEATURE_CONFIGURED);
     }
 
-    @SubscribeEvent
-    public void onPotentialSpawn(WorldEvent.PotentialSpawns event) {
-        IWorld iworld = event.getWorld();
-        if (iworld instanceof World) {
-            if (RFToolsDim.MODID.equals(((World) iworld).getDimensionKey().getLocation().getNamespace())) {
-                event.getList().add(new MobSpawnInfo.Spawners(BlobModule.DIMENSIONAL_BLOB.get(), 300, 6, 20));
-            }
-        }
+//    private MobSpawnInfo.Spawners blobEntry = null;
+//
+//    @SubscribeEvent
+//    public void onPotentialSpawn(WorldEvent.PotentialSpawns event) {
+//        IWorld iworld = event.getWorld();
+//        if (iworld instanceof World) {
+//            if (blobEntry == null) {
+//                blobEntry = new MobSpawnInfo.Spawners(BlobModule.DIMENSIONAL_BLOB.get(), 300, 6, 20);
+//            }
+//            if (RFToolsDim.MODID.equals(((World) iworld).getDimensionKey().getLocation().getNamespace())) {
+//                event.getList().add(blobEntry);
+//            }
+//        }
+//
+//    }
 
-    }
-
+//    private static List<MobSpawnInfo.Spawners> func_241463_a_(ServerWorld world, StructureManager structureManager, ChunkGenerator generator, EntityClassification entityClassification, BlockPos pos, @Nullable Biome biome) {
+//        return ((entityClassification == EntityClassification.MONSTER)
+//                && (world.getBlockState(pos.down()).getBlock() == Blocks.NETHER_BRICKS)
+//                && structureManager.getStructureStart(pos, false, Structure.FORTRESS).isValid())
+//                ? Structure.FORTRESS.getSpawnList()
+//                : generator.func_230353_a_((biome != null)
+//                            ? biome
+//                            : world.getBiome(pos),
+//                    structureManager, entityClassification, pos);
+//    }
+//
 }
