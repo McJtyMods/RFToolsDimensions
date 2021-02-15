1b11b4a71a49819ba113b4d3969232e2f4909ef4
McJty <mcjty1@gmail.com>
2021-02-15 06:57:38 +0100

    Fixed a bug in the 'forgetinvalid' command

--- src/main/java/mcjty/rftoolsdim/commands/CommandForgetInvalid.java
+++ src/main/java/mcjty/rftoolsdim/commands/CommandForgetInvalid.java
@@ -12,11 +12,14 @@ import mcjty.rftoolsdim.dimension.descriptor.CompiledDescriptor;
 import net.minecraft.command.CommandSource;
 import net.minecraft.command.Commands;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.SharedConstants;
 import net.minecraft.util.text.StringTextComponent;
 import net.minecraft.util.text.TextFormatting;
 import net.minecraft.world.server.ServerWorld;
 
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 public class CommandForgetInvalid implements Command<CommandSource> {
 
@@ -30,9 +33,11 @@ public class CommandForgetInvalid implements Command<CommandSource> {
 
     @Override
     public int run(CommandContext<CommandSource> context) throws CommandSyntaxException {
+        SharedConstants.developmentMode = true;
         ServerWorld world = context.getSource().getWorld();
         PersistantDimensionManager mgr = PersistantDimensionManager.get(world);
-        for (Map.Entry<ResourceLocation, DimensionData> entry : mgr.getData().entrySet()) {
+        Set<Map.Entry<ResourceLocation, DimensionData>> entries = new HashSet<>(mgr.getData().entrySet());
+        for (Map.Entry<ResourceLocation, DimensionData> entry : entries) {
             CompiledDescriptor descriptor = DimensionManager.get().getCompiledDescriptor(world, entry.getKey());
             if (descriptor == null) {
                 mgr.forget(entry.getKey());
